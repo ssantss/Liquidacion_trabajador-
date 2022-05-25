121b7c660cfd123ad8f733faa9df313d8f3657a8
ssantss <santjimenez@utp.edu.co>
2022-05-25 14:46:41 -0500

    se utilizan objetos para obtener las variables

--- js/functions.js
+++ js/functions.js
@@ -3,18 +3,58 @@ const mombre = input.value; */
 
 /* const input_salario = document.getElementById("salario");
 const salario = parseInt(input_salario.value); */
-
+function serialize_form(form) {
+    /* Retorna los valores del form en un objeto */
+  
+    var data = new FormData(form);
+    let results = {};
+    for (const [name, value] of data) {
+      results[name] = value;
+    }
+    return results;
+  }
+  
 
 
 function submit_event (e) {
     e.preventDefault()
     console.log(e)
-    calcular()
-    
+    const data = serialize_form(this);
+    console.log(data)
+    const resultados = calcular2(data.salario_base, data.dias_laborados)  
+    console.log(resultados)
 }
+
+
+
 
 const form = document.getElementById('form')    
 form.addEventListener('submit',submit_event)
+
+function calcular2 (salario,dias_laborados) {
+
+    
+    salario = parseInt(salario);
+    dias_laborados = parseInt(dias_laborados);
+    //
+    const cesantias = Math.round((salario * dias_laborados)/360);
+    const intereses_cesantias = Math.round((cesantias * dias_laborados * 0.12)/360);
+    const prima_de_servicios = Math.round((salario * dias_laborados)/360);
+    const vacaciones = Math.round((salario * dias_laborados)/720);
+    const total_liquidacion = cesantias + intereses_cesantias + prima_de_servicios + vacaciones;
+
+    return {
+        cesantias: cesantias,
+        intereses_cesantias: intereses_cesantias,
+        prima_de_servicios: prima_de_servicios,
+        vacaciones: vacaciones,
+        total_liquidacion: total_liquidacion,
+    }
+
+
+}
+
+
 
 
 function calcular(){
